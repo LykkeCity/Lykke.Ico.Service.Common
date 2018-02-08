362978b740e7ce34a55f9abace60b13480e05a55
Yuriy Plotnikov <yuriy.plotnikov@outlook.com>
2018-02-08 17:39:51 +0300

    Small improvements

--- src/Lykke.Service.IcoCommon.AzureRepositories/PayInAddressEntity.cs
+++ src/Lykke.Service.IcoCommon.AzureRepositories/PayInAddressEntity.cs
@@ -1,9 +1,13 @@
-﻿using Lykke.Service.IcoCommon.Core.Domain.PayInAddresses;
+﻿using Lykke.AzureStorage.Tables;
+using Lykke.AzureStorage.Tables.Entity.Annotation;
+using Lykke.AzureStorage.Tables.Entity.ValueTypesMerging;
+using Lykke.Service.IcoCommon.Core.Domain.PayInAddresses;
 using Microsoft.WindowsAzure.Storage.Table;
 
 namespace Lykke.Service.IcoCommon.AzureRepositories
 {
-    public class PayInAddressEntity : TableEntity, IPayInAddressInfo
+    [ValueTypeMergingStrategyAttribute(ValueTypeMergingStrategy.UpdateAlways)]
+    public class PayInAddressEntity : AzureTableEntity, IPayInAddressInfo
     {
         public string CampaignId { get; set; }
         public string Email { get; set; }
