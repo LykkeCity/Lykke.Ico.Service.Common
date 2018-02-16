37013d68bbd5644bc9307b3163aa76242f847018
Yuriy Plotnikov <yuriy.plotnikov@outlook.com>
2018-02-16 16:03:49 +0300

    Email templates editor

--- src/Lykke.Service.IcoCommon/wwwroot/js/emailTemplates/emailTemplates.js
+++ src/Lykke.Service.IcoCommon/wwwroot/js/emailTemplates/emailTemplates.js
@@ -9,9 +9,14 @@
 
 function emailTemplatesController($http) {
     var vm = this;
+    var templatesUrl = "api/mail/templates";
+    var editor;
 
-    vm.templates = [];
-    vm.currentTemplate = null;
+    vm.templateList = [];
+    vm.template;
+
+    vm.selectTemplate = selectTemplate;
+    vm.save = save;
 
     vm.$onInit = function () {
         initEditor();
@@ -19,14 +24,16 @@ function emailTemplatesController($http) {
     }
 
     function loadTemplates() {
-        $http.get("api/mail/templates").then(function (response) {
-            vm.templates = response.data;
-            setCurrentTemplate(vm.templates[0]);
-        });
+        $http
+            .get(templatesUrl)
+            .then(function (response) {
+                vm.templateList = response.data || [];
+                selectTemplate();
+            });
     }
 
     function initEditor() {
-        vm.editor = monaco.editor.create(document.getElementById('editor'), {
+        editor = monaco.editor.create(document.getElementById('editor'), {
             language: 'html',
             minimap: {
                 enabled: false
@@ -35,10 +42,55 @@ function emailTemplatesController($http) {
         });
     }
 
-    function setCurrentTemplate(value) {
-        vm.currentTemplate = value;
-        if (vm.currentTemplate) {
-            vm.editor.setValue(vm.currentTemplate.body);
+    function selectTemplate(template) {
+        if (!template) {
+            if (!vm.template) {
+                template = vm.templateList[0];
+            } else {
+                template = vm.templateList.find(function (t) {
+                    return t.campaignId == vm.template.campaignId && t.templateId == vm.template.templateId;
+                })
+            }
         }
+
+        vm.template = template;
+        editor.setValue(vm.template && vm.template.body ? vm.template.body : "");
     };
+
+    function save() {
+        if (!vm.template) {
+            return;
+        }
+
+        if (!vm.template.campaignId) {
+            alert("CampaignId must not be null or empty");
+            return;
+        }
+
+        if (!vm.template.templateId) {
+            alert("TemplateId must not be null or empty");
+            return;
+        }
+
+        vm.template.body = editor.getValue();
+
+        if (!vm.template.body) {
+            alert("Body must not be null or empty");
+            return;
+        }
+
+        $http
+            .post(templatesUrl, vm.template)
+            .then(
+                function (response) {
+                    if (vm.template._isNew) {
+                        vm.templateList.push(vm.template);
+                        delete vm.template._isNew;
+                    }
+                    alert("Changes saved!");
+                },
+                function (response) {
+                    alert(response.data.errorMessage);
+                });
+    }
 };
