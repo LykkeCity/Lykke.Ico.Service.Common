fdc3bd3b3f993d8195e9e86ede7538cb0bc55c8d
Yuriy Plotnikov <yuriy.plotnikov@outlook.com>
2018-02-19 11:29:58 +0300

    Update client API

--- client/Lykke.Service.IcoCommon.Client/IIcoCommonServiceClient.cs
+++ client/Lykke.Service.IcoCommon.Client/IIcoCommonServiceClient.cs
@@ -8,6 +8,17 @@ namespace Lykke.Service.IcoCommon.Client
     public interface IIcoCommonServiceClient
     {
         Task AddPayInAddressAsync(PayInAddressModel address, CancellationToken cancellationToken = default(CancellationToken));
+
+        Task SendEmailAsync(EmailDataModel emailData, CancellationToken cancellationToken = default(CancellationToken));
+
+        Task<IList<EmailModel>> GetSentEmailsAsync(string to, string campaignId = null, CancellationToken cancellationToken = default(CancellationToken));
+
+        Task AddOrUpdateEmailTemplateAsync(EmailTemplateModel emailTemplate, CancellationToken cancellationToken = default(CancellationToken));
+
+        Task<IList<EmailTemplateModel>> GetCampaignEmailTemplatesAsync(string campaignId, CancellationToken cancellationToken = default(CancellationToken));
+
+        Task<EmailTemplateModel> GetEmailTemplateAsync(string campaignId, string templateId, CancellationToken cancellationToken = default(CancellationToken));
+
         Task<int> HandleTransactionsAsync(IList<TransactionModel> transactions, CancellationToken cancellationToken = default(CancellationToken));
     }
 }
